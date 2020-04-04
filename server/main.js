b0d84803da837121e968be1801eee74654fe7033
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2020-04-04 16:00:47 +0300

    WIP

--- server/main.js
+++ server/main.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const fs = require('fs')
-require('express-fileupload')
+const fileUpload = require('express-fileupload')
+
 const path = require('path')
 
 const app = express()
+app.use(fileUpload())
+
 const port = 4444
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../client/index.html')))
 
 app.post('/upload', function(req, res) {
-  console.log(req.files.foo); // the uploaded file object
+  console.log('Got a file?')
+  JSON.stringify(req.files, null, 2)
+  res.json({files: req.files})
+  // console.log(req.files.file); // the uploaded file object
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
