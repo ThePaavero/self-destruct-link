3b6790636436d6d007ab60548dfe4cd8f8fb2e29
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2020-04-04 16:12:11 +0300

    Uploading works.

--- server/main.js
+++ server/main.js
@@ -14,8 +14,15 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../client/index.
 app.post('/upload', function(req, res) {
   console.log('Got a file?')
   JSON.stringify(req.files, null, 2)
-  res.json({files: req.files})
-  // console.log(req.files.file); // the uploaded file object
+  const file = req.files.file
+  const directory = __dirname + '/uploads/x-' + Math.round(Math.random() * 99999999999)
+  fs.mkdir(directory)
+  file.mv(directory + '/' + file.name, (err) => {
+    if (err) {
+      return res.status(500).send(err)
+    }
+    res.send('File uploaded!')
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
