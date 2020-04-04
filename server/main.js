3484d929b170e52ae057c236b126d8a3fbefada2
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2020-04-04 16:24:24 +0300

    WIP

--- server/main.js
+++ server/main.js
@@ -12,17 +12,22 @@ const port = 4444
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../client/index.html')))
 
 app.post('/upload', function(req, res) {
-  console.log('Got a file?')
   JSON.stringify(req.files, null, 2)
   const file = req.files.file
-  const directory = __dirname + '/uploads/x-' + Math.round(Math.random() * 99999999999)
+  const randomDirSlug = 'x-' + Math.round(Math.random() * 99999999999)
+  const directory = __dirname + '/uploads/' + randomDirSlug
   fs.mkdir(directory)
   file.mv(directory + '/' + file.name, (err) => {
     if (err) {
       return res.status(500).send(err)
     }
-    res.send('File uploaded!')
+    const url = `http://localhost:${port}/download/${randomDirSlug}`
+    res.send(fs.readFileSync(path.join(__dirname + '/../client/uploaded.html')).toString().replace('[URL]', url))
   })
+})
+
+app.get('/download/:slug', (req, res) => {
+  res.json(req.params)
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
